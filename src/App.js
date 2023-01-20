d9aa25d969a7faa862a651d899e8221960de3fb4
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-21 00:50:56 +0530

    setup chakra provider and route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-  return;
+  return (
+    <Router>
+      <Routes>
+        <Route to={"/"} element />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
