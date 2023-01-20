350895c595a41be159bb5559b034c3c62a7e0fc5
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-21 01:31:01 +0530

    fetch api of exchanges

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { server } from "../index";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Container } from "@chakra-ui/react";
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      const { data } = await axios.get(`${server}/exchanges`);
+      setExchanges(data);
+      setLoading(false);
+    };
+    fetchExchanges();
+  }, []);
+
+  return <Container maxW={"container.xl"}></Container>;
 };
 
 export default Exchanges;
