3a6e7d64c3db7a25de64fdda8427031251859770
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-22 01:46:54 +0530

    modified coindetails page and added some features

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,7 +1,124 @@
-import React from "react";
+import {
+  Badge,
+  Box,
+  Container,
+  HStack,
+  Image,
+  Radio,
+  RadioGroup,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { server } from "../index";
+import ErrorContainer from "./ErrorContainer";
 
 const CoinDetails = () => {
-  return <div>CoinDetails</div>;
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+
+  const params = useParams();
+
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
+        setCoin(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [params.id]);
+
+  if (error) return <ErrorContainer message={"Error while fetching coin"} />;
+
+  return (
+    <Container maxW={"container.lg"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}>
+            good
+          </Box>
+
+          <RadioGroup value={currency} onChange={setCurrency}>
+            <HStack
+              justifyContent={"center"}
+              spacing="8"
+              my={"4"}
+              fontWeight="bold"
+            >
+              <Radio value="inr">
+                <Text fontSize={"3xl"}>₹</Text>
+              </Radio>
+
+              <Radio value="usd">
+                {" "}
+                <Text fontSize={"3xl"}>$</Text>
+              </Radio>
+
+              <Radio value="eur">
+                {" "}
+                <Text fontSize={"3xl"}>€</Text>
+              </Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack spacing={"4"} p="16" alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf="center" opacity={0.7}>
+              Last Updated on{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
+            </Text>
+
+            <Image src={coin.image.large} w="16" h={"16"} objectFit="contain" />
+
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+
+              <StatNumber>
+                {currencySymbol}
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
+
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
+                />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+
+            <Badge
+              fontSize={"2xl"}
+              bgColor="blackAlpha.900"
+              color={"white"}
+            >{`#${coin.market_cap_rank}`}</Badge>
+          </VStack>
+        </>
+      )}
+    </Container>
+  );
 };
 
 export default CoinDetails;
