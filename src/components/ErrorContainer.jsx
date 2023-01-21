1da60ae5e55db20ce57fbff7575b908f94a980bb
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-22 01:04:46 +0530

    modified error container

--- src/components/ErrorContainer.jsx
+++ src/components/ErrorContainer.jsx
@@ -1,7 +1,20 @@
+import { Alert, AlertIcon } from "@chakra-ui/react";
 import React from "react";
 
 const ErrorContainer = (props) => {
-  return <div>{props.message}</div>;
+  return (
+    <Alert
+      status="error"
+      position={"fixed"}
+      bottom="4"
+      left={"50%"}
+      transform="translateX(-50%)"
+      w={"container.lg"}
+    >
+      <AlertIcon />
+      {props.message}
+    </Alert>
+  );
 };
 
 export default ErrorContainer;
