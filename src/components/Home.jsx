4138c17a993b5b2597cd46e976696838cc972903
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-01-23 01:13:17 +0530

    modified home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,28 @@
+import { Box, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import btcn from "../assets/btc.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box bgColor={"blackAlpha.900"} w="full" h={"85vh"}>
+      <Image
+        w={"full"}
+        h="full"
+        objectFit={"contain"}
+        src={btcn}
+        filter="grayscale(1)"
+      />
+      <Text
+        fontSize={"7xl"}
+        textAlign="center"
+        fontWeight={"thin"}
+        color="whiteAlpha.700"
+        mt={"-28"}
+      >
+        CryptX
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
