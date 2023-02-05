89c564c62836658061a5ff0cc4f6f5776518f406
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2023-02-05 23:35:39 +0530

    added dark theme by default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+    initialColorMode: "dark",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
